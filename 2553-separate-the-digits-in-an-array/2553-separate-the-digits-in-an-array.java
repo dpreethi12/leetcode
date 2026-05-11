1f48209b1905bbce47d9778b24b60c5c0634e1a5
DEEVANAPELLI PREETHI <134215461+dpreethi12@users.noreply.github.com>
2026-05-11 11:22:52 +0530

    Time: 3 ms (90.15%), Space: 46.9 MB (15.69%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,16 +1,29 @@
 class Solution {
-    public int[] separateDigits(int[] nums) {
-        ArrayList<String> l = new ArrayList<>();
-        int n = 0;
-        for(int i = 0; i < nums.length; i++) {
-            String str = Integer.toString(nums[i]);
-            l.addAll(Arrays.asList(str.split("")));
-            n += str.length();
+    public int countDigits(int n ) {
+        int cnt = 0;
+        while(n > 0) {
+            n = n / 10;
+            cnt++;
         }
-        int arr[] = new int[n];
-        int k = 0;
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(l.get(i));
+        return cnt;
+    }
+    public int[] separateDigits(int[] nums) {
+        int total  = 0;
+        for(int num : nums) {
+            total += countDigits(num);
+        }
+
+        int arr[] = new int[total];
+        int idx = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            int len = countDigits(nums[i]);
+            int div = (int)Math.pow(10, len - 1);
+            while(div > 0) {
+                arr[idx++] = nums[i] / div;
+                nums[i] = nums[i] % div;
+                div = div / 10;
+            }
         }
         return arr;
     }
