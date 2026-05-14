e5ddbc110cff286cb2e5e840670d60c6fb5aea8a
DEEVANAPELLI PREETHI <134215461+dpreethi12@users.noreply.github.com>
2026-05-14 09:47:57 +0530

    Time: 1 ms (91.05%), Space: 44.4 MB (86.29%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length;
+        int count[] = new int[n];
+        Arrays.fill(count, 0);
+        for(int i = 0; i < n; i++) {
+            if(nums[i] >= n) return false;
+            count[nums[i]]++;
+        }
+        for(int i = 1; i < n - 1; i++){
+            if(count[i] != 1) return false;
+        }
+        if(count[n-1] != 2) return false;
+        return true;
+    }
+
+}
