191b185bbe0b09e5523ce2bc944d865b1568a978
DEEVANAPELLI PREETHI <134215461+dpreethi12@users.noreply.github.com>
2026-05-19 13:15:31 +0530

    Time: 2 ms (87.87%), Space: 48 MB (22.66%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,21 +1,12 @@
 class Solution {
-    boolean dp[];
-    boolean dfs(int nums[], int n, int idx) {
-        if(idx == n - 1) return true;
-        if(!dp[idx]) return false;
-        for(int j = 1; j <= nums[idx]; j++){    
-            if(idx + j < n)
-                if(dfs(nums, n, idx + j)){
-                    return true;
-                }else {
-                    dp[idx + j] = false;
-                }
-        }
-        return false;
-    }
     public boolean canJump(int[] nums) {
-        dp = new boolean[nums.length];
-        Arrays.fill(dp, true);
-        return dfs(nums, nums.length, 0);
+        int max = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            if(i > max) return false;
+            max = Math.max(max, i + nums[i]);
+            if(max >= n) return true;
+        }
+        return true;
     }
 }
