9e571522cda47f43d9aa0a1c7a057231728ef659
DEEVANAPELLI PREETHI <134215461+dpreethi12@users.noreply.github.com>
2026-05-19 14:53:43 +0530

    Time: 1 ms (99.57%), Space: 47.4 MB (20.36%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,30 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        int len = 0;
-        boolean vis[] = new boolean[n];
-        while(!q.isEmpty()) {
-            int s = q.size();
-            for(int k = 0; k < s; k++) {
-                int idx = q.poll();
-                if(idx == n - 1) return len ;
-
-                for(int j = 1; j <= nums[idx]; j++) {
-                    if(j + idx < n){
-                        if(j + idx == n -1) return len + 1;
-                        if(!vis[j + idx]){
-                            vis[idx + j] = true;
-                            q.add(j + idx);
-                        }
-                    }
-                }
+        int end = 0, far = 0, len = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            far = Math.max(far, i + nums[i]);
+            if(i == end) {
+                len++;
+                end = far;
             }
-            len++;
-            
         }
-        return len + 1;
+        return len;
     }
 }
