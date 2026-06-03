cb752be129a6a805b18df2ef53b6ba2f3335335f
DEEVANAPELLI PREETHI <134215461+dpreethi12@users.noreply.github.com>
2026-06-03 14:40:26 +0530

    Time: 11 ms (25.86%), Space: 100.7 MB (20.69%) - LeetHub

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int earliestFinishTime(int[] landS, int[] landD, int[] waterS, int[] waterD) {
+        int ans = Integer.MAX_VALUE;
+        int lmin = Integer.MAX_VALUE;
+        for(int i = 0; i < landS.length; i++){
+            lmin = Math.min(landS[i] + landD[i], lmin);
+        }
+        System.out.println(lmin);
+        for(int j = 0; j < waterS.length; j++) {
+            int k = 0;
+            if(waterS[j] < lmin) {
+                k = lmin + waterD[j];
+            } else {
+                k = waterS[j] + waterD[j];
+            }
+            ans = Math.min(k, ans);
+        }
+        System.out.println(lmin);
+        int wmin = Integer.MAX_VALUE;
+        for(int i = 0; i < waterS.length; i++){
+            wmin = Math.min(waterS[i] + waterD[i], wmin);
+        }
+        System.out.println(wmin);
+        for(int j = 0; j < landS.length; j++) {
+            int k = 0;
+            if(landS[j] < wmin) {
+                k = wmin + landD[j];
+            } else {
+                k = landS[j] + landD[j];
+            }
+            ans = Math.min(k, ans);
+        }
+        System.out.println(wmin);
+        return ans;
+    }
+}
